cae0e14216298fcdf11c673e51ca6af19293c189
Vincent Ngeno <khalifngeno@gmail.com>
2017-10-30 17:18:49 +0300

    add title to the webpage, add a header section and styling

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// var divisible3 = function(input){
+// 	if input%3===0;
+// 	return true};
+// 	else {
+// 		return false;
+// 	}
+
+// var divisible5 = function(input){
+// 	if input%5===0;
+// 	return true};
+// 	else {
+// 		return false;
+// 	}
+// var divisible3n5 = function(input){
+// 	if (input%3===0) && (input%5===0){
+// 		return true;
+// 	}
+// 	else {
+// 		return false;
+// 	}
+
+
+// $(document).ready(function(){
+    
+// 	$("#form").submit(function(event){
+// 		var number= $("input").val();
+
+// 		$("#output").append("<li>"+number+"</li>");
+
+// 		event.preventDefault();
+
+// 	});
+
+
+// });
